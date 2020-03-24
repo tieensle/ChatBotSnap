08b36c3af6069f89ea54a20058aff7f8d94a1956
MaetamongVH <lvt11072000@gmail.com>
2020-03-24 10:47:59 +0700

    add first

--- index.js
+++ index.js
@@ -3,31 +3,9 @@ const app = express();
 
 app.use(express.json());
 
-// Creates the endpoint for our webhook
-app.post("/webhook", (req, res) => {
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-});
 app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN =
-    "EAACvlUm8ZBlQBALZBW8hfQvfkQgIPgflLQwbNZC0MhkZAK0IBc98Q2itAwjGdSljBeEYdPbE1wVvBjZBhrRKQXdpN7B5MV7eSdZCQDhG0G0KAoQ3VJTuZAZClHSdROj0CFZB3QRPsZAkVThQttyhhOVuaFT3KPJd59tISeuRABlBsgMgZDZD";
+  let VERIFY_TOKEN = "thisisatoken1234";
 
   // Parse the query params
   let mode = req.query["hub.mode"];
@@ -48,4 +26,24 @@ app.get("/webhook", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 5000, () => console.log("Webhook is listening"));
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+});
+app.listen(process.env.PORT || 1337, () => console.log("Webhook is listening"));
