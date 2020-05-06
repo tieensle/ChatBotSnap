633b00de62d5a9d9484042adaf0002bfc63bf9c6
MaetamongVH <lvt11072000@gmail.com>
2020-05-06 09:15:26 +0700

    add new webhook

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
-"use strict";
+const express = require("express");
+const app = express();
 
-// Imports dependencies and set up http server
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  app = express().use(bodyParser.json()); // creates express http server
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+const PORT = process.env.PORT || 1337;
 
-// Creates the endpoint for our webhook
+app.get("/", (req, res) => {
+  res.send("Server is running, OK!");
+});
+
 app.post("/webhook", (req, res) => {
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+  if (req.body.object === "page") {
+    req.body.entry.forEach((entry) => {
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
     });
-
-    // Returns a '200 OK' response to all requests
     res.status(200).send("EVENT_RECEIVED");
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 });
 
-// Adds support for GET requests to our webhook
 app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "thisisatoken1234";
-
-  // Parse the query params
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-  // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
+    if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+      console.log("webhook is verified!");
       res.status(200).send(challenge);
     } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
 });
+
+// function sendMessage(senderId, message) {
+//   request({
+//     url: "https://graph.facebook.com/v2.6/me/messages",
+//     qs: {
+//       access_token: process.env.PAGE_ACCESS_TOKEN,
+//     },
+//     method: "POST",
+//     json: {
+//       recipient: {
+//         id: senderId,
+//       },
+//       message: {
+//         text: message,
+//       },
+//     },
+//   });
+// }
+
+app.listen(PORT, () =>
+  console.log(`Chat bot server listening at http://localhost:${PORT}`)
+);
