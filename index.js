985bde52d3d158df875f8186d96fcfdeedd2e840
MaetamongVH <lvt11072000@gmail.com>
2020-05-06 13:56:53 +0700

    fix webhook

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function callSendAPI(senderId, message) {
         id: senderId,
       },
       message: {
-        text: message,
+        message: message,
       },
     },
   });
@@ -257,29 +257,29 @@ app.listen(PORT, () =>
 //   callSendAPI(sender_psid, response);
 // }
 
-// function callSendAPI(sender_psid, response) {
-//   // Construct the message body
-//   let request_body = {
-//     recipient: {
-//       id: sender_psid,
-//     },
-//     message: response,
-//   };
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
 
-//   // Send the HTTP request to the Messenger Platform
-//   request(
-//     {
-//       uri: "https://graph.facebook.com/v2.6/me/messages",
-//       qs: { access_token: PAGE_ACCESS_TOKEN },
-//       method: "POST",
-//       json: request_body,
-//     },
-//     (err, res, body) => {
-//       if (!err) {
-//         console.log("message sent!");
-//       } else {
-//         console.error("Unable to send message:" + err);
-//       }
-//     }
-//   );
-// }
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
