c13bd876532811a4da7f9a7b3cd9ece2a695056a
MaetamongVH <lvt11072000@gmail.com>
2020-05-06 14:26:25 +0700

    complete basic webhook

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function handleMessage(senderId, message) {
           template_type: "generic",
           elements: [
             {
-              title: "Đây có phải cái ảnh m gửi không?",
-              subtitle: "Đúng chưa?",
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
               image_url: attachment_url,
               buttons: [
                 {
